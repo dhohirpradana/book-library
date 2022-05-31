562ae05dae2b9565a494092ea496cfda6fb4e5cc
dhohirpradana <dhohirkudus@gmail.com>
2022-05-31 07:14:53 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import { Navigation } from "react-minimal-side-navigation";
+import "react-minimal-side-navigation/lib/ReactMinimalSideNavigation.css";
+import EmailIcon from "@mui/icons-material/Email";
+import { Box, Stack, Typography } from "@mui/material";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Stack direction="row" height="100vh">
+        <Box width={{ sm: "37%", md: "20%" }}>
+          <Navigation
+            // you can use your own router's api to get pathname
+            activeItemId="/management/members"
+            onSelect={({ itemId }) => {
+              // maybe push to the route
+            }}
+            items={[
+              {
+                title: "Dashboard",
+                itemId: "/dashboard",
+                // you can use your own custom Icon component as well
+                // icon is optional
+                elemBefore: () => <EmailIcon />,
+              },
+              {
+                title: "Management",
+                itemId: "/management",
+                elemBefore: () => <EmailIcon />,
+                subNav: [
+                  {
+                    title: "Projects",
+                    itemId: "/management/projects",
+                  },
+                  {
+                    title: "Members",
+                    itemId: "/management/members",
+                  },
+                ],
+              },
+              {
+                title: "Another Item",
+                itemId: "/another",
+                subNav: [
+                  {
+                    title: "Teams",
+                    itemId: "/management/teams",
+                  },
+                ],
+              },
+            ]}
+          />
+        </Box>
+        <Typography m="auto">Hello</Typography>
+      </Stack>
+    </>
   );
 }
 
